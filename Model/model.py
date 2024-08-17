7de4a18ae04ea454a00e4bd6755d65fd169fa866
Senilka Karunarathna <senilka0108@gmail.com>
2024-08-17 16:42:33 +0530

    model

--- Model/model.py
+++ Model/model.py
@@ -37,38 +37,32 @@ def split_and_convert_bp(data):
 def predict_sleep_quality(model, data):
     """Uses the model to predict sleep quality based on the provided data."""
     features = [data[field] for field in ['gender', 'age', 'sleepDuration', 'stressLevel', 'bmiCategory', 'heartRate', 'Systolic_BP', 'Diastolic_BP']]
-    features = np.array(features).reshape(1, -1)  # Reshape for single prediction
-    prediction = model.predict(features)[0]  # Assuming model.predict returns a list
+    features = np.array(features).reshape(1, -1)  
+    prediction = model.predict(features)[0]  
     return prediction.item() 
 
 def process_data(model, data, encoded_gender):
     """Processes data by updating gender to encoded value and adding a success message."""
     try:
-        data['gender'] = encoded_gender  # Overwrite the original gender with its encoded value
+        data['gender'] = encoded_gender  
         if 'sleepDuration' in data:
-            # Parse the ISO 8601 date string as timezone-aware
             duration_datetime = datetime.fromisoformat(data['sleepDuration'].replace('Z', '+00:00'))
-            # Define epoch as timezone-aware
             epoch = datetime(1970, 1, 1, tzinfo=timezone.utc)
-            # Convert total duration to hours
             total_seconds = (duration_datetime - epoch).total_seconds()
             total_hours = total_seconds / 3600 
-            # Format to one decimal place
             data['sleepDuration'] = f"{total_hours:.1f}"
         if 'bloodPressure' in data:
             split_and_convert_bp(data)
 
-        # Predict sleep quality
         sleep_quality = predict_sleep_quality(model, data)
         return {'sleepQuality': sleep_quality}
     
     except Exception as e:
         return {'error': f'Error processing data: {str(e)}'}
 
-# Usage
 if __name__ == "__main__":
-    model_path = '/Users/senilkakarunarathna/Documents/Projects/Sleep Tracker/sleep-tracker-app/Model/model.pkl'
-    encoder_path = '/Users/senilkakarunarathna/Documents/Projects/Sleep Tracker/sleep-tracker-app/Model/label_encoder.pkl'
+    model_path = '../model.pkl'
+    encoder_path = '../label_encoder.pkl'
 
     try:
         model = load_model(model_path)
